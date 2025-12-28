86bfd206ea9f42a1128dd01612950869ab00c2e5
sajad <samidev778@gmail.com>
2025-12-28 12:43:20 +0330

    feat: added header

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+            </h1>
+          </header>
+        </div>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1>{searchTerm}</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
