f8ba5e18aeac27452f61e0fcbe9ab5cd1b3c558c
sajad <samidev778@gmail.com>
2025-12-30 19:29:25 +0330

    fix: upate image path

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" />
+          <img src="/hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
           </h1>
