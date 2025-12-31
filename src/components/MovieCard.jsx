d041b711312fa771e83c0138a7c36480585cd14d
sajad <samidev778@gmail.com>
2025-12-31 14:02:51 +0330

    fix: update image path

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ const MovieCard = ({
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="/star.svg" alt="Star Icon" />
+            <img src="./star.svg" alt="Star Icon" />
             <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
           <span>•</span>
